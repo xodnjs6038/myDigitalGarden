1150d31a237e9dcaeee9222e4fd8a26e84ffdd2d
이태원 <79821555+xodnjs6038@users.noreply.github.com>
2024-04-23 15:02:10 +0900

    스크립트 수정

--- js/layout.js
+++ js/layout.js
@@ -1,18 +1,20 @@
-const modal = document.querySelector(".modal");
-const img = document.querySelector(".img");
-const modal_img = document.querySelector(".modal_content");
-const span = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal");
+  const img = document.querySelector(".img");
+  const modal_img = document.querySelector(".modal_content");
+  const span = document.querySelector(".close");
 
-img.addEventListener("click", () => {
-  modalDisplay("block");
-  modal_img.src = img.src;
+  img.addEventListener("click", () => {
+    modalDisplay("block");
+    modal_img.src = img.src;
+  });
+  span.addEventListener("click", () => {
+    modalDisplay("none");
+  });
+  modal.addEventListener("click", () => {
+    modalDisplay("none");
+  });
+  function modalDisplay(text) {
+    modal.style.display = text;
+  }
 });
-span.addEventListener("click", () => {
-  modalDisplay("none");
-});
-modal.addEventListener("click", () => {
-  modalDisplay("none");
-});
-function modalDisplay(text) {
-  modal.style.display = text;
-}
