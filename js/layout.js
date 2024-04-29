12f60ed1e21df47592f5ae7c26de348114a28c7d
이태원 <79821555+xodnjs6038@users.noreply.github.com>
2024-04-29 10:59:21 +0900

    내용 업데이트

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
+/*
+* nav 상단 고정
+*/
+  var nav = document.querySelector("nav");
+  var wrapper = document.querySelector(".wrapper")
+  window.addEventListener("scroll", function() {
+    var scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (scrollPosition > nav.offsetTop) {
+        nav.style.position = "fixed";
+        nav.style.top = "0";
+        wrapper.style.marginTop = nav.offsetHeight * 3 + "px";
+    } else {
+        nav.style.position = "static";
+        wrapper.style.marginTop = "0";
+    }
+  });
+
+
+/* 
+* 이미지 클릭 시 모달로 열림 
+* 해당 이미지에 img 클래스 추가
+*/
+
   const modal = document.querySelector(".modal");
   const modal_img = document.querySelector(".modal_content");
   const span = document.querySelector(".close");
